c954b1db4dd65525be0846810850b79a72c0ee44
allenINO03 <111472758+allenINO03@users.noreply.github.com>
2023-05-25 13:33:21 +0800

    Add files via upload

---
+++ model/Darwin.java
@@ -0,0 +1,19 @@
+package model;
+
+public class Darwin extends Classmate{
+
+    public void setRating(String string) {
+    }
+
+    public void setTaste(String string) {
+    }
+
+    public String getRating() {
+        return null;
+    }
+
+    public String getTaste() {
+        return null;
+    }
+
+}
